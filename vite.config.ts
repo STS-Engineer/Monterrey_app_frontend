188d875c6fd0491e897c021e908996eac56f97eb
STS-Engineer <administration.sts@avocarbon.com>
2025-06-24 16:13:24 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,31 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     svgr({
       svgrOptions: {
         icon: true,
+        // This will transform your SVG to a React component
+        exportType: "named",
+        namedExport: "ReactComponent",
       },
-      include: "**/*.svg",
-      exportAsDefault: true,
     }),
+    
   ],
-  build: {
-    assetsInlineLimit: 0,
-    manifest: true,
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-      }
-    }
-  },
-  server: {
-    headers: {
-      "Content-Type": "application/javascript",
-    },
-  }
 });
